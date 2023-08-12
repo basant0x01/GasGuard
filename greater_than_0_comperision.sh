8303359d37a1e4337c0fdbf3c4b688dfdb495d32
Basant Karki <123530150+basant0x01@users.noreply.github.com>
2023-08-12 20:50:36 +0545

    Update greater_than_0_comperision.sh

--- greater_than_0_comperision.sh
+++ greater_than_0_comperision.sh
@@ -1,4 +1,5 @@
-echo -e "\n\n\e[32mGAS: GREATER THAN 0 COMPERISION\e[0m"
+echo -e "\n\n\e[32mTITLE: GREATER THAN 0 COMPERISION\e[0m"
+echo -e "\e[32mFILE: $input_file\e[0m"
 grep -nE '>[[:space:]]*0' "$input_file" | while IFS= read -r line; do
         echo "-----------------------------------------------------------------------------"
         echo "LINE: ${line/        /}"  # Remove 8 spaces before "for"
