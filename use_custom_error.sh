e6f12852a431d8f5f73a50a3666d8c8c6bcf52e1
Basant Karki <123530150+basant0x01@users.noreply.github.com>
2023-08-12 20:51:16 +0545

    Update use_custom_error.sh

--- use_custom_error.sh
+++ use_custom_error.sh
@@ -1,10 +1,11 @@
-echo -e "\n\n\e[32mGAS: USE CUSTOM ERROR FOR OUTPUT\e[0m"
+echo -e "\n\n\e[32mTITLE: USE CUSTOM ERROR FOR OUTPUT\e[0m"
+echo -e "\e[32mFILE: $input_file\e[0m"
 grep -nE 'require\s*\([^"]*"[^"]*"[^"]*\)' "$input_file" | while IFS= read -r line; do
         echo "-----------------------------------------------------------------------------"
         echo "LINE: ${line/        /}"  # Remove 8 spaces before "for"
         echo "-----------------------------------------------------------------------------"
 done
-echo -e "\nRECOMMENDATION: Instead of using message in require(), use custom error output for save gas"
+echo -e "\nRECOMMENDATION: Instead of using message in require(), use custom error\noutput for save gas"
 echo -e "\nMODIFY CODE: 
 error InsufficientAuthorize("UnAuthorized")
 require(msg.sender == owner,InsufficientAuthorize)
