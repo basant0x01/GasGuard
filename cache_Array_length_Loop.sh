45d9681e2b93eed550eb57dde361fcf7137442c3
Basant Karki <123530150+basant0x01@users.noreply.github.com>
2023-08-12 20:49:34 +0545

    Update cache_Array_length_Loop.sh

--- cache_Array_length_Loop.sh
+++ cache_Array_length_Loop.sh
@@ -1,4 +1,5 @@
-echo -e "\n\n\e[32mGAS: CACHE ARRAY LENGTH OUTSIDE OF LOOP\e[0m"
+echo -e "\n\n\e[32mTITLE: CACHE ARRAY LENGTH OUTSIDE OF LOOP\e[0m"
+echo -e "\e[32mFILE: $input_file\e[0m"
 grep -nE 'for *\(.*\.length.*\)' "$input_file" | while IFS= read -r line; do
         echo "-----------------------------------------------------------------------------"
         echo "LINE: ${line/        /}"  # Remove 8 spaces before "for"
