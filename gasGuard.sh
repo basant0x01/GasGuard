e065c1b06b66404e5d4d30f2ff43ead1fbb28c84
Basant Karki <123530150+basant0x01@users.noreply.github.com>
2023-08-14 02:40:55 +0545

    Update gasGuard.sh
    Updated: Supports multiple files

--- gasGuard.sh
+++ gasGuard.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 clear
-figlet -c Gas Guard
-echo "                     Scripted by basant0x01 | Initial v0.1"
+figlet -c "Gas Guard"
+echo "                     Scripted by basant0x01 | Initial v0.2"
 
 function engine() {
     vulnTemplates=(
@@ -14,35 +14,45 @@ function engine() {
         "bit_shifting_division_multiplication.sh" 
         "calldata_instead_memory_in_function.sh" 
         "assembly_for_address0.sh"
-        )
+    )
 
-    for tmp in "${vulnTemplates[@]}"; do
-        source "$tmp"
-        echo "*****************************************************************************"
+    for template in "${vulnTemplates[@]}"; do
+        output=$(source "$template" 2>&1)
+        if echo "$output" | grep -q "LINE:"; then
+            echo "$output"
+            echo "*****************************************************************************"
+        fi
     done
 }
 
 # Parse command line arguments
-while [[ $# -gt 0 ]]; do
-    key="$1"
+input_files=()
 
-    case $key in
+while [[ $# -gt 0 ]]; do
+    case $1 in
         -i|--input)
-            input_file="$2"
             shift
+            input_files+=("$1")
+            ;;
+        -mi|--multi-input)
             shift
+            input_files+=("$@")
+            break
             ;;
         *)
             echo "UNKNOWN OPTION: $1"
             exit 1
             ;;
     esac
+    shift
 done
 
-if [ -z "$input_file" ]; then
-    echo "Usage: $0 -i input_file"
+if [ ${#input_files[@]} -eq 0 ]; then
+    echo "Usage: $0 [-i input_file1 ...] [-mi input_file1 ...]"
     exit 1
 fi
 
 # Check for loop issues
-engine
+for input_file in "${input_files[@]}"; do
+    engine
+done
